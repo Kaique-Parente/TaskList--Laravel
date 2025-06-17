9918e9ee2686fb7212803a5132a24e8da52b4475
Kaique-Parente <kaique.parente.game@outlook.com>
2025-06-17 16:20:08 -0300

    [ADD] Aprendendo sobre rotas

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/halle', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/saysHello/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return "Não existe nada aqui!";
+});
