0b717437b6ddf6a7ce99c8092b48f6e3c3b0912a
Kaique-Parente <kaique.parente.game@outlook.com>
2025-06-18 13:05:24 -0300

    [ADD] Rotas para exibir o meu formulário e uma rota para o post.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,10 @@ Route::get('/tasks', function () {
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+;
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', [
         'task' => \App\Models\Task::findOrFail( $id )
@@ -36,6 +41,10 @@ Route::get('/tasks/{id}', function ($id) {
 Route::fallback(function () {
     return "Não existe nada aqui!";
 });
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('task.store');
 
 // Route::get('/hello', function () {
 //     return 'Hello';
