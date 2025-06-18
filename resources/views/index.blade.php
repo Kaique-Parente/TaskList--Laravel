5d910f046dc0942a47ce33ff70e1eabd1436c16a
Kaique-Parente <kaique.parente.game@outlook.com>
2025-06-18 17:12:19 -0300

    [ADD] Pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,17 @@
 @section('title', 'This is my home page')
 
 @section('content')
-@forelse ($tasks as $task)
-    <div>
-        <a href="{{ route('tasks.show', ['task' => $task]) }}">{{ $task->title }}</a>
-    </div>
-@empty
-    <p>Não existe nada aqui ainda</p>
-@endforelse
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['task' => $task]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <p>Não existe nada aqui ainda</p>
+    @endforelse
+
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
